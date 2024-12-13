b35711102de74a758cff1940787db37557c6bb8d
Pongsakan-fluk <pongsakan.obr@futurelifestyle.co.th>
2024-12-13 16:08:09 +0700

    Update wishlist-page

--- src/pages/wishlist.jsx
+++ src/pages/wishlist.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { listWishlist } from "../functions/wishlist";
+//Functions
+import { deleteWishlists, listWishlist } from "../functions/wishlist";
+//Redux
+import { useSelector, useDispatch } from "react-redux";
+import { removeWistlist } from "../store/wishlistSlice";
 
 function wishlist() {
   const [data, setData] = useState([]);
@@ -8,14 +12,29 @@ function wishlist() {
   const [count, setCount] = useState(1);
   const idtoken = localStorage.token;
 
+  const dispatch = useDispatch();
+  const { wishlist } = useSelector((state) => state.wishlistStore);
+
   const fetchData = () => {
     listWishlist(idtoken)
-      .then((res) => res.json())
       .then((res) => {
-        setData(res.response);
+        setData(res.data);
       })
       .catch((err) => {
         console.log("Get wishlist fail!" + err);
+      });
+  };
+
+  const deleteWishlist = (id) => {
+    deleteWishlists(idtoken, id)
+      .then((res) => {
+        const result = wishlist.filter((item) => item.id != res.data.id);
+        dispatch(removeWistlist(result));
+
+        fetchData();
+      })
+      .catch((err) => {
+        console.log(err);
       });
   };
 
@@ -54,7 +73,6 @@ function wishlist() {
 
   return (
     <div className="flex flex-col min-h-screen">
-
       <div className="w-100 grow my-52 container mx-auto">
         <h2 className="text-center text-3xl my-10 py-5 bg-red-100">
           My wishlist
@@ -62,32 +80,43 @@ function wishlist() {
         {data ? (
           data.map((item) => {
             return (
-              <div className="hover:bg-slate-50 border-y flex flex-col mx-auto items-center my-5 py-5 text-center space-y-5 md:flex-row justify-around" key={item.product_id}>
+              <div
+                className="hover:bg-slate-50 border-y flex flex-col mx-auto items-center my-5 py-5 text-center space-y-5 md:flex-row justify-around"
+                key={item.id}
+              >
                 <div>
                   <img
-                    src={`${import.meta.env.VITE_APP_IMAGE}${item.img}`}
-                    alt={item.title}
+                    src={`${import.meta.env.VITE_APP_IMAGE}${
+                      item?.product?.image
+                    }`}
+                    alt={item?.title}
                     className="w-40 mx-auto"
                   />
                 </div>
                 <Link
-                  to={`/shop/${item.product_id}`}
+                  to={`/shop/${item?.productId}`}
                   className="flex flex-col space-y-3 w-64 text-lg"
                 >
-                  <p>{item.title}</p>
-                  <p>${item.price}</p>
+                  <p>{item?.product?.title}</p>
+                  <p>${item?.product?.price}</p>
                 </Link>
-                <div>
+                <div className="space-x-5">
                   <button
                     className="btn btn-outline btn-success z-10"
                     disabled={disable}
-                    onClick={() => addCart(item.product_id)}
+                    onClick={() => addCart(item?.productId)}
                   >
                     {disable ? (
                       <span className="loading " />
                     ) : (
                       <p>Add To Cart</p>
                     )}
+                  </button>
+                  <button
+                    onClick={() => deleteWishlist(item?.id)}
+                    className="btn btn-outline btn-error z-10"
+                  >
+                    X
                   </button>
                 </div>
               </div>
@@ -97,7 +126,6 @@ function wishlist() {
           <p className="text-3xl">No, Products</p>
         )}
       </div>
-
     </div>
   );
 }
